9974d08558378b6fc6f4a3577a41cb92076a77b8
nishananaaah <nishanaf731@gmail.com>
2024-12-11 15:18:50 +0530

    delete-route

--- EXPRESS-MONGOOSE-APIS/ROUTES/contactRoute.js
+++ EXPRESS-MONGOOSE-APIS/ROUTES/contactRoute.js
@@ -73,29 +73,30 @@ router.get("/contact/:id", async (req, res) => {
 router.get("/search", async (req, res) => {
   try {
     const searchTerm = req.query.searchTerm;
-    console.log(searchTerm)
+    console.log(searchTerm);
     const searchRegex = RegExp(searchTerm, "i"); // casesensitive i-converted to lowercase
-    await Contact.find({//match these any of the fields return the data
+    await Contact.find({
+      //match these any of the fields return the data
       $or: [
         { firstName: searchRegex },
         { lastName: searchRegex },
         { emailAddress: searchRegex },
       ],
     })
-    .then((contacts)=>{
-        if(contacts.length){
-            res.status(200).json({contacts:contacts})
-        }else{
-            res.status(500).json({contact:[],message:"No matching record found"})
+      .then((contacts) => {
+        if (contacts.length) {
+          res.status(200).json({ contacts: contacts });
+        } else {
+          res
+            .status(500)
+            .json({ contact: [], message: "No matching record found" });
         }
-        res.status(200).json({contacts:contacts})
-
-
-    })
-    .catch((error)=>{
-        console.log(error)
-        res.status(500).json({message:"Unable to find contacts"})
-    })
+        res.status(200).json({ contacts: contacts });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: "Unable to find contacts" });
+      });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "No matching records found" });
@@ -103,25 +104,52 @@ router.get("/search", async (req, res) => {
 });
 
 //UPDATE - Functionality
-router.put("/contact/:id",async(req,res)=>{
-    try {
-        const id = req.params.id;
-        const updatedContact= req.body;
-        await Contact.findOneAndUpdate({_id:id},updatedContact,{new:true})//if it didnt find anaything creating a new  one
-        .then((updatedContact)=>{
-            console.log(updatedContact);
-            res.status(200).json({message:"Contact updated successfully",contact:updatedContact})
+router.put("/contact/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedContact = req.body;
+    await Contact.findOneAndUpdate({ _id: id }, updatedContact, { new: true }) //if it didnt find anaything creating a new  one
+      .then((updatedContact) => {
+        console.log(updatedContact);
+        res
+          .status(200)
+          .json({
+            message: "Contact updated successfully",
+            contact: updatedContact,
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: "Unable to update the contact" });
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Unable to update the contact" });
+  }
+});
+//Delete Functionality
+//2-Types
+//1-Soft Delete => update the field is "active" => Y/N
+//2-Hard Delete = deletion of the  record/document
 
-        })
-        .catch((error)=>{
-            console.log(error);
-            res.status(500).json({message:"Unable to update the contact"})
-        })
-    } catch (error) {
+router.delete("/contact/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id)
+    await Contact.findByIdAndDelete(id)
+    .then((deletedcontact)=>{
+        console.log(deletedcontact)
+        res.status(200).json({message:"Contact deleted successfully",contact:deletedcontact})
+
+    })
+    .catch((error)=>{
         console.log(error)
-        res.status(500).json({message:"Unable to update the contact"})
-        
-    }
-})
+        res.status(500).json({message:"Unable to delete the contact"})
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Unable to delete the contact" });
+  }
+});
 
 module.exports = router;
